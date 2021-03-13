568c6fab32bf2b36ccf6e3b1b663a37a30abacdf
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-03-14 01:57:55 +0900

    fix: Reset all global recoil states on logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,22 +7,28 @@ import { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { useRecoilValue, useResetRecoilState } from 'recoil'
 import { currentUserState } from 'states/currentUser'
+import { isCheckupValidState } from 'states/isCheckupValid'
+import { todaysHealthDataState } from 'states/todaysHealthData'
 import { userDatasetState } from 'states/userDataset'
 import { auth } from 'utils/firebase'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
   const currentUser = useRecoilValue(currentUserState)
-  const setUserDataset = useSetRecoilState(userDatasetState)
+  const resetUserDataset = useResetRecoilState(userDatasetState)
+  const resetTodaysHealthData = useResetRecoilState(todaysHealthDataState)
+  const resetIsCheckupValid = useResetRecoilState(isCheckupValidState)
 
   useEffect(() => {
     if (currentUser === null) router.push('/login')
   }, [currentUser])
 
   const logout = async () => {
-    setUserDataset(undefined)
+    resetUserDataset()
+    resetTodaysHealthData()
+    resetIsCheckupValid()
     await auth.signOut().catch((err) => alert(err.message))
   }
 
