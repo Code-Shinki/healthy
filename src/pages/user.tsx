a4e4bda19436ce2b762ed8d8eee2cf23e1a46211
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-03-22 22:31:11 +0900

    styles: Styling the user page

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,10 @@
+import { Button, FormControlLabel, Radio, RadioGroup, TextField, Theme } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import Spinner from 'components/Spinner'
 import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { getUserDataset, postUserDataset } from 'requests/userDataset'
 import { currentUserState } from 'states/currentUser'
@@ -11,10 +13,11 @@ import { UserDataset } from 'types/userDataset'
 
 const User: NextPage = () => {
   const router = useRouter()
+  const classes = useStyles()
   const currentUser = useRecoilValue(currentUserState)
   const [userDataset, setUserDataset] = useRecoilState(userDatasetState)
-  const [name, setName] = useState<null | string>(null)
-  const [sex, setSex] = useState<null | string>(null)
+  const [name, setName] = useState<null | string>('')
+  const [gender, setGender] = useState<null | string>(null)
   const [height, setHeight] = useState<null | number>(null)
   const [weight, setWeight] = useState<null | number>(null)
   const [doctor, setDoctor] = useState<null | string>(null)
@@ -27,18 +30,18 @@ const User: NextPage = () => {
     if (!userDataset) return
 
     setName(userDataset.name)
-    setSex(userDataset.sex)
+    setGender(userDataset.gender)
     setHeight(userDataset.height)
     setWeight(userDataset.weight)
     setDoctor(userDataset.doctor)
   }, [userDataset])
 
-  const updateUserInfo = async (event: React.FormEvent<HTMLFormElement>) => {
+  const updateUserInfo = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     if (currentUser) {
       const updateValue = {
         name: name,
-        sex: sex,
+        gender: gender,
         height: height,
         weight: weight,
         doctor: doctor,
@@ -50,65 +53,122 @@ const User: NextPage = () => {
     }
   }
 
+  const changeName = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+  }
+
+  const changeGender = (event: ChangeEvent<HTMLInputElement>) => {
+    setGender(event.target.value)
+  }
+
+  const changeHeight = (event: ChangeEvent<HTMLInputElement>) => {
+    setHeight(Number(event.target.value))
+  }
+
+  const changeWeight = (event: ChangeEvent<HTMLInputElement>) => {
+    setWeight(Number(event.target.value))
+  }
+
+  const changeDoctor = (event: ChangeEvent<HTMLInputElement>) => {
+    setDoctor(event.target.value)
+  }
+
   if (currentUser && userDataset) {
     return (
       <>
         <Head>
           <title>User</title>
         </Head>
-        <form onSubmit={(e) => updateUserInfo(e)}>
-          <div>
-            <label htmlFor="name">ユーザー名</label>
-            <input id="name" type="text" defaultValue={name ? name : ''} onChange={(e) => setName(e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="sex">性別</label>
-            <input
-              type="radio"
-              name="sex"
-              value="男性"
-              checked={sex === '男性'}
-              onChange={(e) => setSex(e.target.value)}
-            />
-            <input
-              type="radio"
-              name="sex"
-              value="女性"
-              checked={sex === '女性'}
-              onChange={(e) => setSex(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="height">身長</label>
-            <input
-              id="height"
-              type="number"
-              step="0.1"
-              defaultValue={height ? height : ''}
-              onChange={(e) => setHeight(Number(e.target.value))}
-            />
-          </div>
-          <div>
-            <label htmlFor="weight">体重</label>
-            <input
-              id="weight"
-              type="number"
-              step="0.1"
-              defaultValue={weight ? weight : ''}
-              onChange={(e) => setWeight(Number(e.target.value))}
-            />
-          </div>
-          <div>
-            <label htmlFor="doctor">かかりつけ医：</label>
-            <input
-              id="doctor"
-              type="text"
-              defaultValue={doctor ? doctor : ''}
-              onChange={(e) => setDoctor(e.target.value)}
-            />
-          </div>
-          <button type="submit">更新</button>
-        </form>
+        <div className={classes.container}>
+          <h1 className={classes.h1}>ユーザー設定</h1>
+          <form className={classes.form} noValidate onSubmit={updateUserInfo}>
+            <div className={classes.wrapper}>
+              <label htmlFor="name" className={classes.label}>
+                ユーザー名
+              </label>
+              <TextField
+                id="name"
+                name="name"
+                value={name ? name : ''}
+                autoComplete="name"
+                required
+                fullWidth
+                margin="dense"
+                onChange={changeName}
+                className={classes.input}
+              />
+            </div>
+            <div className={classes.wrapper}>
+              <label htmlFor="gender" className={classes.label}>
+                性別
+              </label>
+              <RadioGroup aria-label="gender" name="gender" row onChange={changeGender} className={classes.radio}>
+                <FormControlLabel
+                  value="男性"
+                  control={<Radio color="primary" checked={gender === '男性'} />}
+                  label="男性"
+                />
+                <FormControlLabel
+                  value="女性"
+                  control={<Radio color="primary" checked={gender === '女性'} />}
+                  label="女性"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.wrapper}>
+              <label htmlFor="height" className={classes.label}>
+                身長
+              </label>
+              <TextField
+                id="height"
+                name="height"
+                type="number"
+                inputProps={{ step: '0.1' }}
+                value={height ? height : ''}
+                required
+                fullWidth
+                margin="dense"
+                onChange={changeHeight}
+                className={classes.input}
+              />
+            </div>
+            <div className={classes.wrapper}>
+              <label htmlFor="weight" className={classes.label}>
+                体重
+              </label>
+              <TextField
+                id="weight"
+                name="weight"
+                type="number"
+                inputProps={{ step: '0.1' }}
+                value={weight ? weight : ''}
+                required
+                fullWidth
+                margin="dense"
+                onChange={changeWeight}
+                className={classes.input}
+              />
+            </div>
+            <div className={classes.wrapper}>
+              <label htmlFor="doctor" className={classes.label}>
+                かかりつけ医
+              </label>
+              <TextField
+                id="doctor"
+                name="doctor"
+                value={doctor ? doctor : ''}
+                required
+                fullWidth
+                margin="dense"
+                onChange={changeDoctor}
+                className={classes.input}
+              />
+            </div>
+            <Button type="submit" variant="outlined" color="primary" size="large" className={classes.submit}>
+              更新する
+            </Button>
+          </form>
+        </div>
       </>
     )
   }
@@ -117,6 +177,65 @@ const User: NextPage = () => {
 }
 
 export default User
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    padding: '2em 1em',
+    margin: '4em 1em 0',
+    background: '#fff',
+    borderRadius: 5,
+    boxShadow: '0 1px 4px rgba(0, 0, 0, .15)',
+    position: 'relative',
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: 800,
+      padding: '45px',
+      margin: '4em auto 0',
+    },
+  },
+  h1: {
+    width: '90%',
+    padding: '.4em .2em',
+    margin: 0,
+    color: '#fff',
+    fontSize: '1.8em',
+    fontWeight: 'normal',
+    textAlign: 'center',
+    background: 'var(--c-primary)',
+    borderRadius: 5,
+    boxShadow: '0 4px 20px 0 rgba(0, 0, 0, .15), 0 7px 10px -5px rgba(0, 0, 0, .15)',
+    position: 'absolute',
+    top: 0,
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  label: {
+    padding: '0 0 0 .3em',
+    color: 'var(--c-primary)',
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+  },
+  wrapper: {
+    margin: '2em 0',
+  },
+  input: {
+    '& input': {
+      padding: '.8em .5em',
+    },
+  },
+  radio: {
+    margin: '8px 0 4px',
+    '& svg': {
+      fontSize: '2.3rem',
+    },
+  },
+  submit: {
+    margin: '0 auto',
+    display: 'block',
+  },
+}))
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
