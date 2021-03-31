4dc1d76a8dc60e8f10a1727bceb144e3072803ae
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-04-01 01:54:53 +0900

    fix: "willAdvance is not a function" (react-spring) in production env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const { createSecureHeaders } = require('next-secure-headers')
 
 module.exports = {
+  // react-spring issue | https://github.com/pmndrs/react-spring/issues/1078
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /react-spring/,
+      sideEffects: true,
+    })
+    return config
+  },
   // X-Powered-Byヘッダを削除
   poweredByHeader: false,
   // next-secure-headers | https://github.com/jagaapple/next-secure-headers
