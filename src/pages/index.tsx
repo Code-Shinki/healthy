61d01676c2d4fa977f1864c64fc5d16ba463270c
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-04-06 00:44:11 +0900

    feat: Add top page and 404 page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,57 @@
+import { Button, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import VpnKeyIcon from '@material-ui/icons/VpnKey'
+import SvgLogo from 'components/atoms/svg-logo'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
+import styles from 'styles/pages/index.module.scss'
 
 const Top: NextPage = () => {
+  const classes = useStyles()
+
   return (
     <>
       <Head>
         <title>Top</title>
       </Head>
-      <ul>
-        <li>
+      <div className={styles.root}>
+        <Grid container justify="center" className={styles.container}>
+          <SvgLogo styles={styles.logo} />
+          <h1 className={styles.title}>Healthy</h1>
           <Link href="/login">
-            <a>/login</a>
+            <Button size="large" variant="outlined" component="a" className={classes.button}>
+              <VpnKeyIcon className={classes.icon} />
+              ログイン
+            </Button>
           </Link>
-        </li>
-        <li>
           <Link href="/signup">
-            <a>/signup</a>
+            <Button size="large" variant="outlined" component="a" className={`${classes.button} ${styles.banner}`}>
+              <PersonAddIcon className={classes.icon} />
+              新規登録
+            </Button>
           </Link>
-        </li>
-        <li>
-          <Link href="/dashboard">
-            <a>/dashboard</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/checkup">
-            <a>/checkup</a>
-          </Link>
-        </li>
-      </ul>
+        </Grid>
+      </div>
     </>
   )
 }
 
 export default Top
+
+const useStyles = makeStyles(() => ({
+  button: {
+    padding: '.5em 2em',
+    margin: '1em 2em',
+    color: 'var(--c-white)',
+    fontSize: '1.1em',
+    fontWeight: 'bold',
+    border: '2px solid var(--c-white)',
+  },
+  icon: {
+    margin: '0 .5em 0 0',
+    fontSize: '1.8em',
+  },
+}))
