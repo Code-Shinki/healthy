bd2c2d9c22172f7d03395c954e6383854c7fa9e8
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-04-06 01:53:01 +0900

    test: Add snapshot test for static pages

--- test/pages/404.test.tsx
+++ test/pages/404.test.tsx
@@ -1,13 +1,13 @@
 import { cleanup, render } from '@testing-library/react'
 import { useRouter } from 'next/router'
 import React from 'react'
-import Top from '../../src/pages/index'
+import Error from '../../src/pages/404'
 import { nextRouterMock } from '../mocks/nextRouterMock'
 
 // モック関数を作成
 jest.mock('next/router', () => ({ useRouter: jest.fn() }))
 
-describe(`トップページ (/)`, () => {
+describe(`404ページ (/404)`, () => {
   afterEach(cleanup)
 
   test('スナップショット', async () => {
@@ -15,7 +15,7 @@ describe(`トップページ (/)`, () => {
     const mockRouter = nextRouterMock
     ;(useRouter as jest.Mock).mockReturnValue(mockRouter)
 
-    const { asFragment } = render(<Top />)
+    const { asFragment } = render(<Error />)
     const tree = asFragment()
     expect(tree).toMatchSnapshot()
   })
